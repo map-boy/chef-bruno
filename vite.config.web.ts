415c8ac900c5f1db218bcb8bc840f96f4c1bde1e
map-boy <umwemubi@gmail.com>
2026-05-10 21:07:25 +0200

    Fix vite configs for web and app builds

--- vite.config.web.ts
+++ vite.config.web.ts
@@ -1,12 +1,12 @@
-﻿import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, 'src-app'),
+  root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: path.resolve(__dirname, 'dist-app'),
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
 });
